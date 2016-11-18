e7479b6f2bea391c294303db90e5acfb47ce43da
mcassar <mark.cassar@me.com>
2016-11-18 11:55:17 +1100

    Update diskreport.ps1

--- diskreport.ps1
+++ diskreport.ps1
@@ -3,6 +3,6 @@ $EmailTo = "blah@blah.com"
 $Subject = " storage report"
 $servers = @("SERVERNAME","SERVERNAME")
 $body = foreach ($s in $servers) {Get-WmiObject win32_logicaldisk -computername $s | ft -auto SystemName, DeviceID, @{ Label = "Size(GB)" ; Expression = { "{0:N2}" -f ($_.Size/1024/1024/1024) } },@{ Label ="FreeSpace(GB)" ; Expression = { "{0:N2}" -f ($_.FreeSpace/1024/1024/1024) } } |ft |Out-String}
-$SMTPServer = "mail.netregistry.net"
+$SMTPServer = "mail"
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 25) 
 $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
